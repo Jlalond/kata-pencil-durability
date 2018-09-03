8153e0db8971c8cb21ac87087505b24cf8de61e1
jjjlalonde@gmail.com <jjjlalonde@gmail.com>
2018-09-03 12:56:07 -0400

    Upper case cost two points

--- src/PencilDurabilityKata/PencilDurabilityKata.Tests/PencilTests.cs
+++ src/PencilDurabilityKata/PencilDurabilityKata.Tests/PencilTests.cs
@@ -1,4 +1,6 @@
-﻿using PencilDurabilityKata.WritingUtensils;
+﻿using System;
+using System.Text;
+using PencilDurabilityKata.WritingUtensils;
 using Xunit;
 
 namespace PencilDurabilityKata.Tests
@@ -10,6 +12,41 @@ namespace PencilDurabilityKata.Tests
         {
             var pencil = new Pencil(0);
             Assert.Equal(' ', pencil.WriteCharacterIfSharp('J'));
+        }
+
+        [Fact]
+        public void DullPencilReturnsWhiteSpaceMultipleTimes()
+        {
+            var pencil = new Pencil(0);
+            var stringbuilder = new StringBuilder();
+            for (var i = 0; i < 5; i++)
+            {
+                stringbuilder.Append(pencil.WriteCharacterIfSharp('j'));
+            }
+
+            Assert.Equal("     ", stringbuilder.ToString());
+        }
+
+        [Fact]
+        public void WhiteSpaceUsesNoDurability()
+        {
+            var pencil = new Pencil(1);
+            var stringbuilder = new StringBuilder();
+            stringbuilder.Append(pencil.WriteCharacterIfSharp(' '));
+            stringbuilder.Append(pencil.WriteCharacterIfSharp('j'));
+
+            Assert.Equal(" j", stringbuilder.ToString());
+        }
+
+        [Fact]
+        public void UpperCaseLettersTakeTwoDurabilityPoints()
+        {
+            var pencil = new Pencil(2);
+            var stringbuilder = new StringBuilder();
+            stringbuilder.Append(pencil.WriteCharacterIfSharp('J'));
+            stringbuilder.Append(pencil.WriteCharacterIfSharp('j'));
+
+            Assert.Equal("J ", stringbuilder.ToString());
         }
     }
 }
--- src/PencilDurabilityKata/PencilDurabilityKata/WritingUtensils/Pencil.cs
+++ src/PencilDurabilityKata/PencilDurabilityKata/WritingUtensils/Pencil.cs
@@ -1,11 +1,13 @@
 ﻿using System;
+using System.Linq;
 
 namespace PencilDurabilityKata.WritingUtensils
 {
     public class Pencil
     {
         private int _durabilityRating;
-        private char _whiteSpace = ' ';
+        private const char WhiteSpace = ' ';
+        private const int UpperCaseCost = 2;
 
         public Pencil(int durabilityRating)
         {
@@ -14,13 +16,34 @@ namespace PencilDurabilityKata.WritingUtensils
 
         public char WriteCharacterIfSharp(char characterToWrite)
         {
+            if (IsWhiteSpaceOrNewLine(characterToWrite))
+            {
+                return characterToWrite;
+            }
+
+            if (IsUpperCase(characterToWrite) && _durabilityRating >= UpperCaseCost)
+            {
+                _durabilityRating -= UpperCaseCost;
+                return characterToWrite;
+            }
+
             if (_durabilityRating > 0)
             {
                 _durabilityRating--;
                 return characterToWrite;
             }
 
-            return _whiteSpace;
+            return WhiteSpace;
+        }
+
+        private static bool IsWhiteSpaceOrNewLine(char character)
+        {
+            return character == ' ' || character == '\n';
+        }
+
+        private static bool IsUpperCase(char character)
+        {
+            return char.IsUpper(character);
         }
     }
 }
