3bd32f74e28662de90393dae7dada1d2c252870d
jjjlalonde@gmail.com <jjjlalonde@gmail.com>
2018-09-03 13:32:05 -0400

    Erase tests are going well, but I'm going outside to watch this epic thunderstorm:

--- src/PencilDurabilityKata/PencilDurabilityKata.Tests/EraserTests.cs
+++ src/PencilDurabilityKata/PencilDurabilityKata.Tests/EraserTests.cs
@@ -19,5 +19,29 @@ namespace PencilDurabilityKata.Tests
             Assert.Equal("JJJJJ", stringBuilder.ToString());
         }
 
+        [Fact]
+        public void EraseReturnsAllWhiteSpace()
+        {
+            var eraser = new Eraser(0);
+            var stringBuilder = new StringBuilder();
+            for (var i = 0; i < 5; i++)
+            {
+                stringBuilder.Append(eraser.Erase('j'));
+            }
+
+            Assert.Equal("     ", stringBuilder.ToString());
+        }
+
+        [Fact]
+        public void EraseWhiteSpaceHasNoCost()
+        {
+            var eraser = new Eraser(1);
+            var stringBuilder = new StringBuilder();
+            stringBuilder.Append(eraser.Erase(' '));
+            stringBuilder.Append(eraser.Erase('j'));
+            stringBuilder.Append(eraser.Erase(' '));
+
+            Assert.Equal(" j ", stringBuilder.ToString());
+        }
     }
 }
