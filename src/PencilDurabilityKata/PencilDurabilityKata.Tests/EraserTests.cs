853c54eb5611286702bd965b897b6ac3fed87416
jjjlalonde@gmail.com <jjjlalonde@gmail.com>
2018-09-03 13:28:52 -0400

    Initial eraser tests

---
+++ src/PencilDurabilityKata/PencilDurabilityKata.Tests/EraserTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+using PencilDurabilityKata.WritingUtensils;
+using Xunit;
+
+namespace PencilDurabilityKata.Tests
+{
+    public class EraserTests
+    {
+        [Fact]
+        public void EraseReturnsAllWhiteSpace()
+        {
+            var eraser = new Eraser(1000);
+            var stringBuilder = new StringBuilder();
+            for (var i = 0; i < 5; i++)
+            {
+                stringBuilder.Append(eraser.Erase('J'));
+            }
+
+            Assert.Equal("JJJJJ", stringBuilder.ToString());
+        }
+    }
+}
--- src/PencilDurabilityKata/PencilDurabilityKata/WritingUtensils/Eraser.cs
+++ src/PencilDurabilityKata/PencilDurabilityKata/WritingUtensils/Eraser.cs
@@ -2,5 +2,28 @@
 {
     public class Eraser
     {
+        private int _durabilityRating;
+        private const char WhiteSpace = ' ';
+
+        public Eraser(int durabilityRating)
+        {
+            _durabilityRating = durabilityRating;
+        }
+
+        public char Erase(char characterToErase)
+        {
+            if (characterToErase == ' ')
+            {
+                return characterToErase;
+            }
+
+            if (_durabilityRating > 0)
+            {
+                _durabilityRating--;
+                return characterToErase;
+            }
+
+            return WhiteSpace;
+        }
     }
 }
